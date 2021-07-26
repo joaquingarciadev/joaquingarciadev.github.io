755f0bcd067a2512f63e0a3bda7b8c216d61a9d0
Djoako22 <djoako22@gmail.com>
2021-07-26 03:46:23 -0300

    .

--- main.js
+++ main.js
@@ -6,4 +6,43 @@ options.forEach((ele,index)=>{
   });
 });
 
-gsap.from("h1", {duration: 3, x: 300, opacity: 0});
+gsap.timeline({
+  scrollTrigger: {
+    trigger:"section", 
+    start: "center center", 
+    end: "bottom top",
+    // markers: true,
+    scrub: true,
+    // pin:true
+  }
+})
+.to("section",{duration: 2,opacity: 0})
+
+gsap.timeline({
+  scrollTrigger: {
+    trigger:".box1", 
+    start: "center center", 
+    end: "bottom top",
+    // markers: true,
+    scrub: true,
+    pin:true
+  }
+})
+.from(".text1",{x: innerWidth*1, opacity: 0})
+.from(".text2",{x: innerWidth*-1, opacity: 0})
+.from(".text3",{x: innerWidth*1, opacity: 0}) 
+
+gsap.timeline({
+  scrollTrigger: {
+    trigger:".box2", 
+    start: "center center", 
+    end: "bottom top",
+    // markers: true,
+    scrub: true,
+    pin:true
+  }
+})
+.from(".box2",{opacity: 0})
+.from(".text4",{y: innerHeight*1, opacity: 0})
+.from(".text5",{y: innerHeight*1, opacity: 0})
+.from(".text6",{y: innerHeight*1, opacity: 0}) 
