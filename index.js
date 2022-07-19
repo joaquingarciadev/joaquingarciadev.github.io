6e143286082208d43f1fa67b610e48cbf33876d5
unknown <djoako22@gmail.com>
2022-07-19 20:27:43 -0300

    .

--- index.js
+++ index.js
@@ -1,159 +1,160 @@
-document.addEventListener(
-    "scroll",
-    (e) => {
-        if (
-            document.querySelector(".main").scrollTop >=
-            document.querySelector("#contacto").offsetTop - 10
-        ) {
-            if (window.getComputedStyle(document.querySelector(".btn_nav")).display == "none") {
-                document.querySelector("nav > ul").style.border = "#f5f5f5 solid 2px";
-                document.querySelectorAll("nav a").forEach((e) => {
-                    e.classList.add("navdark");
-                });
-            }
-            document.querySelector(".btn_nav").style.color = "#f5f5f5";
-        } else {
-            document.querySelector("nav > ul").style.border = "#111111 solid 2px";
-            document.querySelectorAll("nav a").forEach((e) => {
-                e.classList.remove("navdark");
-            });
-            document.querySelector(".btn_nav").style.color = "#111111";
-        }
-    },
-    true
-);
-
-document.querySelector(".btn_nav").addEventListener("click", (e) => {
-    document.querySelector(".btn_nav").classList.toggle("active");
-    document.querySelectorAll("li > a").forEach((e) => {
-        e.addEventListener("click", (e) => {
-            document.querySelector(".btn_nav").classList.remove("active");
-            document.querySelector("ul").style.visibility = "hidden";
-        });
-    });
-    if (document.querySelector(".btn_nav").classList.contains("active")) {
-        document.querySelector("ul").style.visibility = "visible";
+let previousScroll = window.pageYOffset;
+window.onscroll = function () {
+    // Effect hide and show navbar
+    let currentScroll = window.pageYOffset;
+    if (previousScroll > currentScroll) {
+        document.querySelector("nav").style.top = "0";
     } else {
-        document.querySelector("ul").style.visibility = "hidden";
+        document.querySelector("nav").style.top = "-50px";
     }
-});
+    previousScroll = currentScroll;
+    // Effect shadow navbar
+    if (currentScroll > 100) {
+        document.querySelector("nav").style.boxShadow = "0px 0px 10px 0px var(--second)";
+    } else {
+        document.querySelector("nav").style.boxShadow = "none";
+    }
+};
 
-let habilidades = [
+const showSidebar = () => {
+    document.querySelector(".sidebar").classList.add("show-sidebar");
+    document.querySelector(".overlay").classList.add("show-overlay");
+};
+
+const hideSidebar = () => {
+    document.querySelector(".sidebar").classList.remove("show-sidebar");
+    document.querySelector(".overlay").classList.remove("show-overlay");
+};
+
+const skills = [
     {
-        nombre: "HTML",
+        name: "HTML",
         img: "src/habilidades/html-5.svg",
     },
     {
-        nombre: "CSS",
+        name: "CSS",
         img: "src/habilidades/css-3.svg",
     },
     {
-        nombre: "Javascript",
+        name: "Javascript",
         img: "src/habilidades/js.svg",
     },
     {
-        nombre: "Node JS",
+        name: "Node JS",
         img: "src/habilidades/node-js.svg",
     },
     {
-        nombre: "React",
+        name: "React",
         img: "src/habilidades/react.svg",
     },
     {
-        nombre: "MongoDB",
+        name: "MongoDB",
         img: "src/habilidades/MongoDB.png",
     },
     {
-        nombre: "Git",
+        name: "Git",
         img: "src/habilidades/git.svg",
     },
     {
-        nombre: "PHP",
+        name: "PHP",
         img: "src/habilidades/php.png",
     },
     {
-        nombre: "MySQL",
+        name: "MySQL",
         img: "src/habilidades/sql.png",
     },
     {
-        nombre: "Bootstrap",
+        name: "Bootstrap",
         img: "src/habilidades/bootstrap.svg",
     },
     {
-        nombre: "Wordpress",
+        name: "Wordpress",
         img: "src/habilidades/wordpress.png",
     },
     {
-        nombre: "Web Sraping",
+        name: "Web Sraping",
         img: "src/habilidades/data.png",
     },
 ];
-let proyectos = [
+
+const projects = [
     {
-        nombre: "Login Starter",
-        descripcion: "Tecnologias </br> NodeJS, Express, React, Bootstrap y MongoDB",
+        name: "Login Starter",
+        description: "Login para una aplicación web",
+        skills: ["HTML", "CSS", "Javascript", "React", "Node JS", "MongoDB"],
         img: "src/proyectos/loginmern.png",
         demo: "https://login-djoako22.vercel.app/",
         repo: "https://login-djoako22.vercel.app/",
     },
     {
-        nombre: "Pedidos",
-        descripcion:
-            "App para hacer pedidos a un numero de telefono espcifico </br></br> Tecnologias </br> NodeJS, Express",
+        name: "Pedidos",
+        description: "App para que tus clientes puedan hacer pedidos a tu local",
+        skills: ["HTML", "CSS", "Javascript", "Node JS"],
         img: "src/proyectos/App Menus.png",
         demo: "https://djoako22.github.io/app_pedidos",
         repo: "https://github.com/Djoako22/app_pedidos",
     },
     {
-        nombre: "Cripto Alarma",
-        descripcion:
-            "Alarma de precio de criptomonedas </br></br> Tecnologias </br> NodeJS, Express",
+        name: "Cripto Alarma",
+        description: "Alarma de precio de criptomonedas",
+        skills: ["HTML", "CSS", "Javascript", "Node JS"],
         img: "src/proyectos/Cripto Alarma.png",
         demo: "https://djoako22.github.io/cripto_alarma/",
         repo: "https://github.com/Djoako22/cripto_alarma",
     },
     {
-        nombre: "Juego Cajero",
-        descripcion:
-            "Juego para practicar y divertirte en el manejo de la caja </br></br> Tecnologias </br> NodeJS, Express",
+        name: "Juego Cajero",
+        description: "Juego para practicar y divertirte en el manejo de la caja",
+        skills: ["HTML", "CSS", "Javascript", "Node JS"],
         img: "src/proyectos/Juego Cajero.png",
         demo: "https://djoako22.github.io/juego_cajero/",
         repo: "https://github.com/Djoako22/juego_cajero",
     },
     {
-        nombre: "Que comemos?",
-        descripcion:
-            "App que te elije un plato de comida aleatoria </br></br> Tecnologias </br> NodeJS, Express",
+        name: "Que comemos?",
+        description: "App que te elije un plato de comida aleatoria",
+        skills: ["HTML", "CSS", "Javascript", "Node JS"],
         img: "src/proyectos/Que Comemos.png",
         demo: "https://djoako22.github.io/que_comemos/",
         repo: "https://github.com/Djoako22/que_comemos",
     },
 ];
 
-const div_habilidades = document.querySelector(".habilidades");
-const div_proyectos = document.querySelector(".proyectos");
+const skills_label = document.querySelector(".skills");
+const projects_label = document.querySelector(".projects");
 
 function render() {
-    // Habilidades
-    habilidades.forEach((habilidad) => {
-        div_habilidades.innerHTML += `
-        <div class="habilidad" >
-            <img src=${habilidad.img} width='50' style="border-radius: 50%;">
-            ${habilidad.nombre}
+    // Skills
+    skills.forEach((skill) => {
+        skills_label.innerHTML += `
+        <div class="skill" >
+            <img src=${skill.img} width='50' arial-label="${skill.name}">
+            ${skill.name}
         </div>
         `;
     });
-    // Proyectos
-    proyectos.forEach((proyecto) => {
-        div_proyectos.innerHTML += `
-        <div class="proyecto">
-            <img src="${proyecto.img}" alt="">
-            <div class="contenido">
-                <h3>${proyecto.nombre}</h3>
-                <p>${proyecto.descripcion}</p>
-                <div class="enlaces">
-                    <a href="${proyecto.repo}" aria-label="REPO"><i class="uil uil-github"></i></a>
-                    <a href="${proyecto.demo}" aria-label="DEMO"><i class="uil uil-play-circle"></i></a>
+    // Projects
+    projects.forEach((project) => {
+        projects_label.innerHTML += `
+        <div class="card">
+            <div class="card-img">
+                <img src="${project.img}" alt="" width="50"/>
+                <filter/>
+            </div>
+            <div class="card-content">
+                <h3>${project.name}</h3>
+                <p>${project.description}</p>
+                <div class="card-icons">
+                    ${project.skills.map((skill) => {
+                        const skill_ = skills.find((skill_) => skill_.name === skill);
+                        return `<img src=${skill_.img} width='20' style="border-radius: 50%;" arial-label="${skill_.name}">`;
+                    })}
+                </div>
+                <div class="card-links">
+                    <a href="${project.repo}" aria-label="REPO"><i class="uil uil-github"></i></a>
+                    <a href="${
+                        project.demo
+                    }" aria-label="DEMO"><i class="uil uil-play-circle"></i></a>
                 </div>
             </div>
         </div>
