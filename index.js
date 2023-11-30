ea5f1cabf9928b7e0dadc88d56229c71fbd2a451
Joaquin Garcia <djoako22@gmail.com>
2023-11-30 00:07:55 -0300

    I added new media and corrected some styles

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// Tawk.to z-index
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/60f0b0b7649e0a0a5ccf2b7e/1f9q9q9q0";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
+
+
 // Load animation
 const fadeOut = () => {
   const loaderWrapper = document.querySelector(".wrapper");
